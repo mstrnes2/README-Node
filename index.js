f99d895ec46ee1e8ee8eaf2e213347bc0c2e1ff8
Molly Starnes <mstrnes2@gmail.com>
2023-08-29 13:24:06 -0500

    added code for index and generatemarkdown

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // TODO: Include packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer');
+const markdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -30,9 +31,10 @@ const questions = [
         message: 'What is this project used for?',
     },
     {
-        type: 'input',
+        type: 'list',
         name: 'license',
         message: 'What license are you using for this project?',
+        choices: ['agpl', 'apache', 'mit', 'no license']
     },
     {
         type: 'input',
@@ -42,20 +44,25 @@ const questions = [
     {
         type: 'input',
         name: 'test',
-        message: 'What are all the different test that can be performed in this project?'
-    },
-    {
-        type: 'input',
-        name: 'questions',
-        message: ''
+        message: 'Please provide instructions on how to test the app.'
     }
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) =>
+    err ? console.error(err) : console.log(`${fileName} file generated!`));
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((answers) => {
+        const readmeContent = generateMarkdown(answers);
+
+        writeToFile('README.md', readmeContent);
+    });
+}
+
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,79 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (data.license === agpl){
+    return `[![License: AGPL v3](https://img.shields.io/badge/License-AGPL_v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)`;
+  }else if (data.license === apache){
+    return `[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`;
+  }else if (data.license === mit){
+    return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;
+  }else{
+    return ``;
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (data.license === agpl){
+    return '[GNU AGPLv3 License](https://www.gnu.org/licenses/agpl-3.0.en.html)';
+  }else if (data.license === apache){
+    return '[Apache License 2.0](https://www.apache.org/licenses/LICENSE-2.0)';
+  }else if (data.license === mit){
+    return '[MIT License](https://opensource.org/licenses/MIT)';
+  }else{
+    return ``;
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license !== 'No License') {
+    return `This project is licensed under the ${license === 'mit' ? 'an' : 'a'} ${license.toUpperCase()} License.`;
+  }else {
+    return ``;
+  }
+}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+function generateMarkdown(answers) {
+  const licenseBadge = renderLicenseBadge(answers.license);
+  const licenseLink = renderLicenseLink(answers.license);
+  const licenseSection = renderLicenseSection(answers.license);
 
+  return `
+  # ${answers.title}
+
+  ## Description
+
+  ${answers.description}
+
+  ## Table of Contents
+
+  ${answers.tableOfContents}
+
+  ## Installation
+
+  ${answers.installation}
+
+  ## Usage
+
+  ${answers.usage}
+
+  ## License
+
+  ${answers.licenseBadge}
+  ${answers.licenseLink}
+  ${answers.licenseSection}
+
+  ## Contributing
+
+  ${answers.contributing}
+
+  ## Test
+
+  ${answers.test}
 `;
 }
 
